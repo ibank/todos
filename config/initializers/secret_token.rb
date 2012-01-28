cafa4a95f54ee1dfa4bafcf09f7826bcf4526af7
Benjamin Lee <ibank@Dae-Sung-Leeui-MacBook-Air.local>
2012-01-28 20:42:35 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = '759527c697dce8fd373ce32348b1c7d0dc7089d5b2761a4e6117d46134b01273d75a823ca74360e64b40aacd837d504a5a843a592a0087365ebacb7dadec91fb'
